f05610bd35153c852d637e5f259659c58d0a05ce
JudahSan <baraka.judah@yahoo.com>
2022-12-22 00:51:46 +0300

    adding image field to model

--- django_into_env/mysite/edibles/models.py
+++ django_into_env/mysite/edibles/models.py
@@ -9,3 +9,4 @@ class Item(models.Model):
   item_name = models.CharField(max_length=200)
   item_desc = models.CharField(max_length=200)
   item_price = models.IntegerField()
+  item_image =  models.CharField(max_length=500, default="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRA5L8IbQ846lWwlpYLaguWNOP3mSSE0klyig&usqp=CAU")
