cad36877c344696aec1175f9158e24fcc95b6d9d
JudahSan <baraka.judah@yahoo.com>
2022-12-23 15:18:57 +0300

    add edit/update functionality

--- django_into_env/mysite/edibles/urls.py
+++ django_into_env/mysite/edibles/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('<int:item_id>', views.detail, name = 'detail'),
     # add items
     path('add', views.create_item, name='create_item'),
+    # edit
+    path('update/<int:item_id>', views.update_item, name='update_item' ),
 ]
