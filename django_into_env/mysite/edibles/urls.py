3a6d28126ea2ab8ed463d67c6f47ace52fd7650b
JudahSan <baraka.judah@yahoo.com>
2022-12-25 09:53:12 +0300

    implementing class based detail view

--- django_into_env/mysite/edibles/urls.py
+++ django_into_env/mysite/edibles/urls.py
@@ -5,10 +5,11 @@ from . import views
 app_name = 'edibles'
 
 urlpatterns = [
+    # edibles
     path('', views.IndexClassView.as_view(), name='index'),
     path('item/', views.item, name='item'),
-    path('checkout/', views.checkout, name='checkout'),
-    path('<int:item_id>', views.detail, name = 'detail'),
+    # path('checkout/', views.checkout, name='checkout'),
+    path('<int:pk>', views.EdiblesDetail.as_view(), name = 'detail'),
     # add items
     path('add', views.create_item, name='create_item'),
     # edit
